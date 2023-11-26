b3a6e01083afdd50f5020df5aa5ad2c75e6db063
Esthera Dow <estheradow@MacBook-Pro-Esthera.local>
2023-11-26 14:06:35 +0100

    add tests, figure out a working config for rt midi

--- src/infrastructure/midi/Midi.py
+++ src/infrastructure/midi/Midi.py
@@ -21,7 +21,7 @@ class Midi:
 
     def __init__(self):
         self.midi_out = rtmidi.MidiOut()
-        self.channel = 0x00
+        self.channel = 0x0
 
     def set_channel(self, new_channel: int) -> None:
         self.channel = new_channel
@@ -29,33 +29,33 @@ class Midi:
     def list_ports(self) -> list[str]:
         return self.midi_out.get_ports()
 
-    def open_port(self, port_number: int) -> None:
+    def open_midi_port(self, port_number: int) -> None:
         self.midi_out.open_port(port_number)
 
-    def send_message(self, status: Status, data: Data = None) -> None:
+    def send_channel_message(self, status: Status, data: Data = None) -> None:
+        msg = [status.message_type | status.channel, data.first]
+        print(msg)
+        if data.second is not None:
+            msg.append(data.second)
+
         self.midi_out.send_message(
-            status.message_type,
-            status.channel,
-            data.first,
-            data.second,
+            msg
         )
 
     def send_control_change(self, data: Data) -> None:
-        self.send_message(
+        self.send_channel_message(
             Status(CONTROL_CHANGE, self.channel),
             data,
         )
 
     def send_program_change(self, program: int) -> None:
-        self.send_message(
+        self.send_channel_message(
             Status(PROGRAM_CHANGE, self.channel),
             Data(program)
         )
 
     def send_system_realtime_message(self, message: int) -> None:
-        self.send_message(
-            self.midi_out.send_message([message])
-        )
+        self.midi_out.send_message([message])
 
     def start_song(self) -> None:
         self.send_system_realtime_message(SONG_START)
--- test/RT505Test.py
+++ test/RT505Test.py
@@ -1,10 +1,32 @@
 import unittest
+from time import sleep
+
+from src.application.midi.Rt505 import Rt505
+from src.infrastructure.midi.Midi import Midi
 
 
 class MyTestCase(unittest.TestCase):
-    def test_rt505_should_record_and_play_on_track(self):
-        self.assertEqual(True, False)  # add assertion here
+    midi = Midi()
+    rt505 = Rt505(midi)
 
+    def test_rt505_should_show_as_available_device(self):
+        actual_ports = self.midi.list_ports()
+        expected_port = 'RC-505'
+        self.assertIn(expected_port, actual_ports)
+    def test_rt505_should_record_and_play_on_track(self):
+        print(hex(0xB0 | 0x00))
+        actual_ports = self.midi.list_ports()
+        print(f"found ports: {actual_ports}")
+        self.midi.open_midi_port(1)
+        self.rt505.record(1)
+        sleep(5)
+        self.rt505.play(1)
+        sleep(5)
+        self.rt505.stop(1)
+
+    def test_should_play_all(self):
+        self.midi.open_midi_port(1)
+        self.rt505.play_all()
     def test_rt505_should_play_then_pause_a_track(self):
         self.assertEqual(True, False)  # add assertion here
 
